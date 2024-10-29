7b12bfa3e79d9a6d484df88aa6728bd4e7c951ce
Nandwa Japheth <japhethnandwa7@gmail.com>
2024-10-29 16:06:10 +0300

    included the signedi and out options using clerk and styled the header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
+import Header from "@/components/Header";
 
 
 export const metadata: Metadata = {
@@ -17,7 +18,10 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <body> {children}</body>
+        <body>
+          <Header />
+           {children}
+        </body>
       </html>
     </ClerkProvider>
    
