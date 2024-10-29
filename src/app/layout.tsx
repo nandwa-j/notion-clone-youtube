bacaf1d9f9735b3f0034592ba22755b3dc330d73
Nandwa Japheth <japhethnandwa7@gmail.com>
2024-10-29 15:22:22 +0300

    setup the .env

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 
 import "./globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
 
 
 export const metadata: Metadata = {
@@ -14,9 +15,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body> {children}
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body> {children}</body>
+      </html>
+    </ClerkProvider>
+   
   );
 }
