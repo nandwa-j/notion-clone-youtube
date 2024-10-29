6038f5f90037ac61087cf75b2bf7f3a3a16f8a0f
Nandwa Japheth <japhethnandwa7@gmail.com>
2024-10-29 16:38:26 +0300

    made a sidebar component with a new button

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import Header from "@/components/Header";
+import Sidebar  from "@/components/Sidebar";
 
 
 export const metadata: Metadata = {
@@ -20,7 +21,14 @@ export default function RootLayout({
       <html lang="en">
         <body>
           <Header />
-           {children}
+          <div className="flex min-h-screen">
+            <Sidebar />
+
+            <div className="flex-1 p-4 bg-gray-100 overflow-y-auto scrollbar-hide ">
+              {children}
+              </div>
+          </div >
+           
         </body>
       </html>
     </ClerkProvider>
