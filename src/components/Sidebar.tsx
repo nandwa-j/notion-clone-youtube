b1769b9ee0c60e7b89e10c5049067896aa01902c
Nandwa Japheth <japhethnandwa7@gmail.com>
2024-10-29 17:01:41 +0300

    used shadcn's sheet to dispay the pop up menu

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,8 +1,30 @@
+import { MenuIcon } from 'lucide-react';
 import NewDocumentButton from './NewDocumentButton';
+
+import {
+    Sheet,
+    SheetContent,
+    SheetHeader,
+    SheetTitle,
+    SheetTrigger,
+  } from "@/components/ui/sheet"
 
 function Sidebar() {
   return (<div className='p-2 md:p-5 bg-gray-200 relative'>
-        <NewDocumentButton/>
+        <Sheet>
+            <SheetTrigger>
+                <MenuIcon className='p-2 hover:opacity-30 rounded-lg' size={40} />
+            </SheetTrigger>
+            <SheetContent side="left">
+                <SheetHeader>
+                <SheetTitle>Menu</SheetTitle>
+                <div>options</div>
+                </SheetHeader>
+            </SheetContent>
+            </Sheet>
+        <div className='hidden md:inline'>
+        <NewDocumentButton /> 
+            </div>
     </div>);
 }
 
